a03fc0cb6372ffcc00a8487d60088fb771bbd043
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-03-29 19:28:13 +0300

    add: functions intro

---
+++ 2.functions/2.1_index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+//#region Function declaration
+
+// function render() {
+//     console.log('🟢', 'test render');
+// }
+
+// console.log('🟢', render());
+
+
+
+
+// let email = 'vasia@mail.com';
+// function render() {
+//     let email = 'petya@mail.com';
+//     email = 'dima@mail.com';
+//     console.log('🟢', email);
+// }
+
+// render();
+// console.log('🟢', email);
+
+
+
+
+// let email = 'petya@mail.com';
+// function render() {
+//     email = 'dima@mail.com';
+//     console.log('🟢', email);
+// }
+// render();
+// console.log('🟢', email);
+
+
+
+
+// function render(message) {
+//     console.log('🟢', message);
+// }
+
+// function render(message = 'no_message') {
+//     console.log('🟢', message);
+// }
+
+// render('Vasia');
+// render();
+
+
+
+// function func(a) {
+//     console.log('🟢', a);
+// }
+// function func(a, b) {
+//     console.log('🟢', a, b);
+// }
+
+// func();
+// func(1);
+// func(2, 3);
+// func(5, 6, 7);
+
+
+
+
+// function getRandom() {
+//     return 4;
+// }
+
+// function render(val = getRandom()) {
+//     console.log('🟢', val);
+// }
+
+// console.log('🟢', getRandom());
+// render();
+// render(101);
+// render('Message from script');
+
+
+
+
+
+// function render(message) {
+//     // if (message === undefined)
+//     //     console.log('🟢', 'error');
+//     // else
+//     //     console.log('🟢', message);
+
+//     message ??= 'error';
+//     console.log('🟢', message);
+// }
+
+//#region 
+
+
+
+
+//#region Function expression
+
+// ФУНКЦИЯ - ЭТО ОБЪЕКТ !!!
+
+const user = {
+    id: 101
+};
+
+const render = function() {
+    console.log('🟢', 'test render');
+}
+
+render();
+
+
+
+
+
+
+
+//#endregion
+
+
+
+
