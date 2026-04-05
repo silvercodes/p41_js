1ecfbf73b6585379c468ea56d9bb08a9314aa0a0
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-04-05 15:04:54 +0300

    add: object

--- 3.object/methods.js
+++ 3.object/methods.js
@@ -1,3 +1,4 @@
+'use strict';
 
 //#region Simple call
 
@@ -26,23 +27,313 @@
 
 
 
-const user = {
-    id: 101,
-    email: 'vasia@mail.com',
+// const user = {
+//     id: 101,
+//     email: 'vasia@mail.com',
 
-    func: () => console.log('🟢', 'Arrow function' ),
+//     func: () => console.log('🟢', 'Arrow function' ),
 
-    render: function() {
-        console.log('🟢', 'render');
-    },
+//     render: function() {
+//         console.log('🟢', 'render');
+//     },
 
-    info() {
-        console.log('🟢', 'info');
-    }
-};
+//     info() {
+//         console.log('🟢', 'info');
+//     }
+// };
 
-user.func();
-user.render();
-user.info();
+// user.func();
+// user.render();
+// user.info();
+
+
+
+
+
+
+// const admin = {
+//     id: 101,
+//     email: 'vasia@mail.com',
+
+//     render: function() {
+//         // console.log('🟢', email);                // ERROR
+
+//         console.log('🟢', this.email);
+
+//         // console.log('🟢', user.email);
+
+//         // console.log('🟢', user === this);
+//     }
+// };
+
+// admin.render();
+
+// // const client = user;
+// // client.render();
+
+
+
+
+
+
+
+// function renderInfo() {
+//     console.log('🟢', this.email);
+// }
+
+// // renderInfo();
+
+// const a = {
+//     email: 'vasia@mail.com',
+// };
+
+// const b = {
+//     email: 'petya@mail.com'
+// }
+
+// a.render = renderInfo;
+// b.render = renderInfo;
+
+// a.render();
+// b.render();
+
+
+
+
+
+// const user = {
+//     id: 101,
+//     render() {
+//         // this === user
+
+//         const handler1 = () => console.log('🟢', this.id);
+//         handler1();
+
+//         const handler2 = function() {
+//             console.log('🟢', this.id);
+//         };
+//         handler2();
+//     }
+// };
+
+// user.render();
+
+
+
+
+
+// function builder() {
+//     return {
+//         id: 101,
+//         reference: this,
+//     }
+// }
+
+// // const obj = builder();
+// // console.log('🟢', obj);
+
+// const user = {};
+// user.method = builder;
+// console.log('🟢', user);
+
+// const obj = user.method();
+// console.log('🟢', obj);
+
+
+
+
+
+
+// const counter = {
+//     count: 0,
+//     add() {
+//         // this === counter
+
+//         this.count++;
+
+//         return this;
+//     },
+//     render() {
+//         console.log('🟢', `count = ${this.count}`);
+//     }
+// };
+
+// // counter.add().add().add();
+// // counter.render();
+// // console.log('🟢', counter);
+
+
+// const f = counter.render;
+// f();
+
 
 //#endregion
+
+
+//#region new
+
+// function builder(id, email) {
+//     const obj = {};
+
+//     obj.id = id;
+//     obj.email = email;
+
+//     return obj;
+// }
+
+// const obj = builder(101, 'vasia@mail.com');
+// console.log('🟢', obj);
+
+
+
+
+
+
+// function User(id, email) {
+//     // this = {}                // неявно
+
+//     this.id = id;
+//     this.email = email;
+
+//     // return this;             // неявно
+// }
+
+// // const result = User(202, 'vasia@mail.com');
+// // console.log('🟢', result);                       // ERROR
+
+// const user = new User(101, 'petya@mail.com');
+// console.log('🟢', user);
+
+
+
+
+
+
+// function createUser(id, email) {
+//     this.id = id;
+//     this.email = email;
+// }
+
+// // const a = new createUser(202, 'a@m.c');
+// // console.log('🟢', a);
+
+// const obj = {};
+// obj.buider = createUser;
+// obj.buider(303, 'x@m.c');
+// console.log('🟢', obj);
+
+
+
+
+
+
+// function f() {
+//     console.log('🟢', new.target);
+// }
+
+// f();
+// new f();
+
+
+
+
+// function User(email) {
+//     if (! new.target)
+//         return new User(email);
+
+//     this.email = email;
+// }
+
+// console.log('🟢', new User('vasia@mail.com'));
+// console.log('🟢', User('petya@mail.com'));
+
+
+
+
+
+
+// function User(email) {
+//     this.email = email;
+
+//     // return this;                // неявно
+//     // return {test: true};        // -----> {test: true}
+//     // return 34;                  // -----> this
+//     // return;                     // -----> this
+// }
+
+// console.log('🟢', new User('vasia@mail.com'));
+
+
+
+
+
+// function User(email) {
+//     this.id = 101;
+//     this.email = email;
+
+//     this.render = function() {
+//         console.log('🟢', `id: ${this.id}, email: ${this.email}`);
+//     }
+// }
+
+// const obj = new User('vasia@mail.com');
+// console.log('🟢', obj);
+// obj.render();
+
+// const f = obj.render;
+
+// new f();
+
+// const temp = {
+//     id: 202,
+//     email: 'petya@mail.com',
+//     show: f
+// }
+// temp.show();
+
+
+
+
+
+
+
+// const obj = {};
+
+// function A() {
+//     return obj;
+// }
+
+// function B() {
+//     return obj;
+// }
+
+// const a = new A();
+// const b = new B();
+// console.log('🟢', a === b);
+
+
+
+
+
+
+const counter = {
+    count: 0,
+    add() {
+        this.count++;
+
+        return this;
+    },
+    render() {
+        console.log('🟢', `count = ${this.count}`);
+    }
+}
+
+
+
+
+
+//#endregion
+
+
+
+
