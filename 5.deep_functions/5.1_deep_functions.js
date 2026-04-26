481ab47b35ea60e18f247e2dc2fb718a7c6ab57a
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-04-26 19:00:09 +0300

    add: deep functions

--- 5.deep_functions/5.1_deep_functions.js
+++ 5.deep_functions/5.1_deep_functions.js
@@ -24,18 +24,77 @@
 
 
 
-function creator() {
-    let count = 0;
 
-    return function() {
-        return count++;
+// function creator() {
+//     let count = 0;
+
+//     const func = function() {
+//         console.dir(creator);
+//         return count++;
+//     };
+
+//     return func;
+// }
+
+// const a = creator();
+// const b = creator();
+
+// console.log('🟢', a());
+// console.log('🟢', a());
+// console.log('🟢', a());
+
+// console.log('🟢', b());
+// console.log('🟢', b());
+// console.log('🟢', b());
+
+
+//#endregion
+
+
+
+//#region EXAMPLE 1
+
+
+let a;
+a = 10;
+
+let b = 12;
+
+const f1 = function() {
+    let a;
+    a = 100;
+    let b = 120;
+
+    const f1 = function() {
+        console.log('🟢', a);
+    };
+
+    const f2 = () => console.log('🟢', b);
+
+    f1();
+
+    function f0() {
+        let c = 100;
+        console.log('🟢', 'c', c);
+        console.log('🟢', 'b', b);
+        // console.log('🟢', 'x', x);
     }
+
+    f0();
+};
+
+const f2 = () => console.log('🟢', b);
+
+f1();
+
+function f0() {
+    let c = 100;
+    console.log('🟢', 'c', c);
+    console.log('🟢', 'b', b);
+    console.log('🟢', 'x', x);
 }
 
-const a = creator();
-const b = creator();
-
-
+f0();
 
 
 
